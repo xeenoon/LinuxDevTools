f76014acea5f64844d4bde1f44c4c9a5f07fe488
xeenoon <ccw100asdf@gmail.com>
2024-04-23 16:18:25 +1000

    Added hexadecimal checker

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 
 #include <iostream>
+using namespace std;
 
 int main() {
-	int64_t input = 0;
+	string strinput;
+	
 	std::cout << "Convert to binary: ";
-	std::cin >> input;
+	std::cin >> strinput;
+
+	if(strinput.rfind("0x",0)==0) //Check for hex
+	{
+	}
+	else
+	{
+		uint64_t input = stoull(strinput);
 	for(int bitidx=0; bitidx<64;++bitidx)
 	{
 		if((((uint64_t)(1)<<(63-bitidx))&input)==0)
@@ -15,10 +24,12 @@ int main() {
 		{
 			std::cout << "1";
 		}
+		std::cout << " ";
 		if((bitidx+1)%8==0)
 		{
 			std::cout << std::endl;
 		}
+	}
 	}
 	return 0;
 }
