03d779fc50757d596a717f1cc12f4684c6d8e7fa
xeenoon <ccw100asdf@gmail.com>
2024-04-23 18:54:32 +1000

    Made program loop

--- main.cpp
+++ main.cpp
@@ -3,36 +3,38 @@
 using namespace std;
 
 int main() {
-	string strinput;
+	while(true)
+	{
+		string strinput;
 
-	std::cout << "Convert  binary: ";
-	std::cin >> strinput;
-	uint64_t input = 0;
-	if(strinput.rfind("0x",0)==0) //Check for hex
-	{
-		input = stoull(strinput.substr(2), NULL, 16);
-	}
-	else
-	{
-		input = stoull(strinput);
-	}
-	for(int bitidx=0; bitidx<64;++bitidx)
-	{
-		if((((uint64_t)(1)<<(63-bitidx))&input)==0)
+		std::cout << "Convert  binary: ";
+		std::cin >> strinput;
+		uint64_t input = 0;
+		if(strinput.rfind("0x",0)==0) //Check for hex
 		{
-			//std::cout << "\x1B[36m0\033[0m\t\t"; //Print cyan
-			 cout << "\033[1;36m0\033[0m";
+			input = stoull(strinput.substr(2), NULL, 16);
 		}
 		else
 		{
-			cout << "\033[1;32m1\033[0m";
-
-			//std::cout << "\x1B[32m1\033[0m\t\t"; //Print green
-		}	
-		std::cout << " ";
-		if((bitidx+1)%8==0)
+			input = stoull(strinput);
+		}
+		for(int bitidx=0; bitidx<64;++bitidx)
 		{
-			std::cout << std::endl;
+			if((((uint64_t)(1)<<(63-bitidx))&input)==0)
+			{
+				//Print cyan
+				cout << "\033[1;36m0\033[0m";
+			}
+			else
+			{
+				cout << "\033[1;32m1\033[0m";
+				//Print green
+			}	
+			std::cout << " ";
+			if((bitidx+1)%8==0)
+			{
+				std::cout << std::endl;
+			}
 		}
 	}
 	return 0;
